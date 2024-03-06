9eeb0aea55a04f3a7f62959dbd175eb79506a246
sj_Li <99625994+lsjBatter@users.noreply.github.com>
2024-03-06 15:41:03 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-export CUDA_VISIBLE_DEVICES=5
-
-python main.py \
---sarimg-dir /data/trainData/image \
---sarmsk-dir /data/trainData/gt \
---trainsarcsv ./data/train.csv \
---validsarcsv ./data/val.csv \
-
-
